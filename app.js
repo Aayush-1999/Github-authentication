38f5990c6e3bde60ebb0d574b2aefbec00e94681
Aayush-1999 <aayushaggarwal2007@gmail.com>
2019-10-23 23:51:35 +0530

    Add animate.css

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 
 const express = require("express"),
       app     = express();
-      mongoose = require("mongoose");
+      mongoose = require("mongoose"),
+      middlware = require("./middleware");
 
 mongoose.connect(process.env.DATABASEURL,{useUnifiedTopology:true, useNewUrlParser:true});
 mongoose.set("useFindAndModify",false);
 mongoose.use("useCreateIndex",true);
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.use("view engine", "ejs");
 
 app.listen(process.env.PORT || 3000){
