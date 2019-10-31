a145eb35f6553159cabc9bd6ddf02f02ce20ae44
Aayush-1999 <aayushaggarwal2007@gmail.com>
2019-10-31 18:13:28 +0530

    Add logn and reigster routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,32 @@
 const express = require("express"),
-      router  = express.Router();
+      router  = express.Router(),
+      passport = require("passport")
+      User    = require("../models/user");
 
 router.get("/",(req,res)=>{
     res.render("signup");
 });
+
+router.post("/register",(req,res)=>{
+    User.create(req.body.user);  
+})
+
+router.post("/login",passport.authenticate('local'{
+    failureRedirect:"/login",
+    failureFlash:"Invalid email or password"
+    }),(req,res)=>{
+        res.redirect("/dashboard");
+    }
+);
+
+router.get("/auth/github",passport.authenticate("github"));
+
+router.get("/auth/github/callback",passport.authenticate("github",{
+    failureRedirect:"/login",
+    failureFlash:true
+    }),(req,res)=>{
+        res.redirect("/dashboard");
+    }
+)
 
 module.exports=router;
